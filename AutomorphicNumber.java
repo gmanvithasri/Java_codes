ecff7b9267aea26711bc4d443d73cd472cb13afb
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-01-21 11:30:32 +0530

    Create AutomorphicNumber.java

---
+++ AutomorphicNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it is an automorphic number: ");
+        int number = scanner.nextInt();
+
+        if (isAutomorphic(number)) {
+            System.out.println(number + " is an automorphic number.");
+        } else {
+            System.out.println(number + " is not an automorphic number.");
+        }
+    }
+
+    private static boolean isAutomorphic(int number) {
+        int square = number * number;
+        
+        while (number > 0) {
+            if (number % 10 != square % 10) {
+                return false;
+            }
+            number /= 10;
+            square /= 10;
+        }
+        return true;
+    }
+}

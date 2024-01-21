2dfbcd8142e7e02c45713535ec7c059a46a4252a
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-01-21 11:15:40 +0530

    Create WordCounter.java

---
+++ WordCounter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string to count its words:");
+        String inputString = scanner.nextLine();
+
+        String[] words = inputString.split("\\s+");
+
+        int wordCount = words.length;
+
+        if(wordCount == 1 && words[0].isEmpty()) {
+            wordCount = 0;
+        }
+
+        System.out.println("Number of words in the string: " + wordCount);
+    }
+}

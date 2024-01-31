9eb8c5b3b3a326db905ebb93901de66892906490
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-01-31 23:05:41 +0530

    Create StringRotationChecker.java

---
+++ StringRotationChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringRotationChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        if (isRotation(str1, str2)) {
+            System.out.println(str2 + " is a rotation of " + str1);
+        } else {
+            System.out.println(str2 + " is not a rotation of " + str1);
+        }
+    }
+
+    private static boolean isRotation(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        String concatenated = str1 + str1;
+        return concatenated.contains(str2);
+    }
+}

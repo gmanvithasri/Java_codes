086cab6d4ab22078f399f292ed8f145ab3c62997
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:34:03 +0530

    IntToString2.java

---
+++ IntToString2.java
@@ -0,0 +1,9 @@
+//Using Integer.toString()
+
+public class IntToString2 {
+    public static void main(String[] args) {
+        int number = 456;
+        String str = Integer.toString(number);
+        System.out.println("The string value is: " + str);
+    }
+}

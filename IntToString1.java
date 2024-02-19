15cdbc3bc71b9eb29621fc447453f524e7790068
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:32:39 +0530

    IntToString1.java

---
+++ IntToString1.java
@@ -0,0 +1,9 @@
+//Using String.valueOf()
+
+public class IntToStringExample {
+    public static void main(String[] args) {
+        int number = 123;
+        String str = String.valueOf(number);
+        System.out.println("The string value is: " + str);
+    }
+}

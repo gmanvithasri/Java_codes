62d0e6c4735af179431feeed7f5016bf997de096
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:50:04 +0530

    LongToString2.java

---
+++ LongToString2.java
@@ -0,0 +1,9 @@
+//Using Long.toString()
+
+public class LongToStringExample2 {
+    public static void main(String[] args) {
+        long number = 987654321L;
+        String str = Long.toString(number);
+        System.out.println("The string value is: " + str);
+    }
+}

8ff4153d7e46aa7f842dbd224a4331f79771eadd
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:49:56 +0530

    LongToString1.java

---
+++ LongToString1.java
@@ -0,0 +1,9 @@
+//Using String.valueOf()
+
+public class LongToStringExample {
+    public static void main(String[] args) {
+        long number = 123456789L;
+        String str = String.valueOf(number);
+        System.out.println("The string value is: " + str);
+    }
+}

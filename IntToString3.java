73ddd322127b6077031d75e02eb0487f9ca42de6
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:40:47 +0530

    IntToString3.java

---
+++ IntToString3.java
@@ -0,0 +1,9 @@
+//Additional Method: String Concatenation
+
+public class IntToStringExample3 {
+    public static void main(String[] args) {
+        int number = 789;
+        String str = number + "";
+        System.out.println("The string value is: " + str);
+    }
+}

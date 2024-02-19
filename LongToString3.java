647ae773877a37d86586ad0c6c2b0660c82c2652
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-19 16:51:20 +0530

    LongToString3.java

---
+++ LongToString3.java
@@ -0,0 +1,9 @@
+//Additional Method: Concatenation with an Empty String
+
+public class LongToString3 {
+    public static void main(String[] args) {
+        long number = 102030405060L;
+        String str = number + "";
+        System.out.println("The string value is: " + str);
+    }
+}

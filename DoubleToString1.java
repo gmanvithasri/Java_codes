ae0bcb9ea8194a39fe96a400fb1cb4ffcbf8e465
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-27 23:41:26 +0530

    DoubleToString1.java

---
+++ DoubleToString1.java
@@ -0,0 +1,7 @@
+public class DoubleToStringExample2 {
+    public static void main(String[] args) {
+        double number = 678.90;
+        String numberStr = Double.toString(number);
+        System.out.println("The string value is: " + numberStr);
+    }
+}

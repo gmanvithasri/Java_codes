61f566da3d16a787cce628836549a55dc6fba953
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-02-27 23:38:12 +0530

    StringToDouble.java

---
+++ StringToDouble.java
@@ -0,0 +1,7 @@
+public class StringToDoubleExample {
+    public static void main(String[] args) {
+        String numberStr = "123.45";
+        double number = Double.parseDouble(numberStr);
+        System.out.println("The double value is: " + number);
+    }
+}

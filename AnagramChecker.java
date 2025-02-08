528f78d2ef11504801084e1a3bc35720aa5b00fd
Manvitha Sri Guthula <108047448+gmanvithasri@users.noreply.github.com>
2025-02-08 22:58:03 +0530

    AnagramChecker.java

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class AnagramChecker {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
